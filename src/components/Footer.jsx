8ce18adbcb508d540d7e998e527c6d973b9cf7a5
thamalBOTCALM <thamal@botcalm.com>
2023-12-11 17:02:47 +0530

    footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -29,49 +29,6 @@ const Footer = () => {
       // }}
     >
       <div className="flex flex-col bg-[#01819D] rounded-t-[60px] xl:px-12 md:px-12 px-6 text-white py-6 space-y-4">
-        <div className="flex flex-row space-x-4 justify-between">
-          <div className="flex flex-col space-y-4">
-            <p className="uppercase xl:text-lg md:text-lg text-sm 2xl:text-xl font-semibold">Links</p>
-            <motion.div variants={textAnimate}>
-              <div className="text-xs 2xl:text-lg space-y-1">
-                <p className="cursor-pointer">Home</p>
-                <p className="cursor-pointer">Service</p>
-                <p className="cursor-pointer">Price</p>
-                <p className="cursor-pointer">About Us</p>
-                <p className="cursor-pointer">Features</p>
-              </div>
-            </motion.div>
-          </div>
-
-          <div className="flex flex-col space-y-4">
-            <p className="uppercase xl:text-lg md:text-lg text-sm 2xl:text-xl font-semibold">
-              Product
-            </p>
-            <motion.div variants={textAnimate}>
-              <div className="text-xs 2xl:text-lg space-y-1">
-              <a href="https://www.winlads.com/conditions"  className="cursor-pointer">Membership T&C</a>
-              <p className="cursor-pointer">Cars</p>
-              <p className="cursor-pointer">Drive</p>
-              <p className="cursor-pointer">Winners</p>
-              </div>
-            </motion.div>
-          </div>
-
-          <div className="flex flex-col space-y-4">
-            <p className="uppercase xl:text-lg md:text-lg text-sm 2xl:text-xl font-semibold">
-              Community
-            </p>
-            <motion.div variants={textAnimate}>
-              <div className="text-xs 2xl:text-lg space-y-1">
-              <p className="cursor-pointer">Global Partners</p>
-              <p className="cursor-pointer">Forum</p>
-              <p className="cursor-pointer">Careers</p>
-              <p className="cursor-pointer">Community</p>
-              <p className="cursor-pointer">Brand Assets</p>
-              </div>
-            </motion.div>
-          </div>
-        </div>
         <div className="flex xl:flex-row flex-col space-y-2 justify-center space-x-8 items-center">
           <div className="flex flex-row items-center">
             <img
