89bebc880c2ff7b6b60f9094d67fcd148f23b599
NimsaraBotCalm <nimsara@botcalm.com>
2024-01-02 18:45:46 +0530

    images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,15 @@ import Logo from "../assets/headerLogo.png";
 const Header = () => {
   return (
     <div id="header" className="flex flex-row justify-between">
-      <div className="flex flex-row justify-between xl:gap-8 md:gap-8 gap-4">
+      <div className="flex flex-row justify-between xl:gap-8 md:gap-8 gap-2">
         <div className="rounded-full px-2 py-1 border border-solid border-[#01819D] items-center justify-center cursor-pointer flex flex-row gap-2">
           <img src={Logo} alt="" className="w-1/3" />
           <p className="text-[#01819D] text-xs xl:text-lg md:text-lg">Winlads</p>
         </div>
         <div className="flex flex-row gap-2 uppercase text-[#01819D] items-center cursor-pointer 2xl:text-lg">
-          <a href="" className="text-md hover:font-bold">login</a>
+          <a href="" className="text-xs md:text-md hover:font-bold">login</a>
           {/* <p className="text-xs hover:font-bold">register</p> */}
-          <p className="text-md hover:font-bold">language</p>
+          <p className="text-xs md:text-md hover:font-bold">language</p>
         </div>
       </div>
 
