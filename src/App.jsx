1b1596632169922501940ba2c583e7ff4f46fefc
SuraMahesh97 <suranga@botcalm.com>
2024-01-03 05:47:01 +0530

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, createHashRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Privacy from "./pages/Privacy";
 import TermsCondition from "./pages/TermsCondition";
 import GoogleAnalytics from "./utils/GoogleAnalytics";
 
 function App() {
-  const router = createBrowserRouter([
+  const router = createHashRouter([
     {
       path: "/",
       element: <Home />,
