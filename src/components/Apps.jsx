e5b24a5554fbccd548a1ca2a4beb23fc5b4429aa
SuraMahesh97 <suranga@botcalm.com>
2024-01-04 23:59:44 +0530

    update

--- src/components/Apps.jsx
+++ src/components/Apps.jsx
@@ -3,64 +3,117 @@ import video from "../assets/video.mp4";
 import BgImg from "../assets/bgCar.png";
 import AppStore from "../assets/appstore.png"
 import PlayStore from "../assets/googleplay.png"
+import { motion } from 'framer-motion';
 
 const Apps = () => {
-  const divStyle = {
-    backgroundImage: `url(${BgImg})`,
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    height: "500px",
-  };
 
   return (
-    <div
-      style={divStyle}
-      className="mt-4 flex py-0 text-center relative justify-center flex-col mb-12"
-    >
-      <video
-        src={video}
-        className="w-full h-full object-cover"
-        autoPlay
-        loop
-        playsInline
-        muted
-      ></video>
-      <div className="bg-gradient-to-t from-black/75 to-black/50 absolute top-0 left-0 w-full h-full" />
+    <div className="w-full relative h-[500px] flex items-center justify-center mb-8">
+    {/* section 01 with ReactPlayer */}
+    {/* <ReactPlayer
+      url='https://player.vimeo.com/video/898036697?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479'
+      playing
+      loop
+      muted
+      width='100%'
+      height='100%'
+      style={{
+        minWidth: '100%',
+        minHeight: '100%',
+      }}
+      className="react-player opacity-50"
+    /> */}
+    <video src={video} className='absolute w-full h-full object-cover top-0 left-0 z-[0]' autoPlay muted loop playsInline></video>
+    <div className='absolute w-full h-full top-0 left-0 z-[1]'>
+      <div className='w-full h-full bg-gradient-to-b to-black from-[rgba(0,0,0,.5)_75%]'></div>
+    </div>
+    <div className="mx-auto 2xl:max-w-[2400px] w-full px-3 lg:px-5 py-10 lg:py-16 z-10  relative">
 
-      <div className="text-center flex justify-center flex-col absolute top-1/2 -translate-y-1/2 left-0 w-full px-5 xl:px-20 space-y-8 xl:space-y-12">
-        <div className="flex flex-row items-center justify-between space-x-4">
-        <div className="flex flex-col space-y-2 items-center">
-          <p className="text-2xl xl:text-4xl text-white">
-            <span className="text-[#43E3ED]">200</span>&nbsp;+
-          </p>
-          <p className="text-white xl:text-sm text-xs capitalize">Australian Business Partners</p>
-        </div>
-        <div className="flex flex-col space-y-2 items-center">
-          <p className="text-white text-2xl xl:text-4xl">
-            <span className="text-[#43E3ED]">350</span>&nbsp;+
-          </p>
-          <p className="text-white capitalize xl:text-sm text-xs">
-            In Saving through our mates rates discount
-          </p>
-        </div>
-        <div className="flex flex-col space-y-2 items-center">
-          <p className="text-white text-2xl xl:text-4xl">
-            <span className="text-[#43E3ED]">200</span>&nbsp;+
-          </p>
-          <p className="text-white capitalize xl:text-sm text-xs">
-            Stores tou can redeem offers in person or online
-          </p>
-        </div>
+      <div className="flex items-center justify-center 2xl:gap-40 xl:gap-36 lg:gap-32 md:gap-8 sm:gap-6 gap-5 lg:flex-row flex-col">
+        <div className="flex flex-col justify-center items-center  ">
+          <motion.p
+            initial={{ opacity: 0, y: "-40%" }}
+            whileInView={{ opacity: 1, y: "0" }}
+            transition={{ duration: 0.5, delay: 0 }}
+            viewport={{ once: true }}
+            className="text-[#00F0FF] font-bold text-2xl lg:text3xl xl:text-4xl 2xl:text-6xl"
+          >
+            200 <span className="text-white">+</span>
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: "40%" }}
+            whileInView={{ opacity: 1, y: "0" }}
+            transition={{ duration: 0.5, delay: 0 }}
+            viewport={{ once: true }}
+            className="text-white text-center text-sm md:text-base 2xl:text-lg"
+          >
+            Australian business partners
+          </motion.p>
         </div>
 
-        <div className="flex flex-row items-center gap-4 xl:gap-8 justify-center">
-            <img src={AppStore} alt="" />
-            <img src={PlayStore} alt="" />
+        <div className="flex flex-col justify-center items-center ">
+          <motion.p
+            initial={{ opacity: 0, y: "-40%" }}
+            whileInView={{ opacity: 1, y: "0" }}
+            transition={{ duration: 0.5, delay: 0.3 }}
+            viewport={{ once: true }}
+            className="text-[#00F0FF] font-bold text-2xl lg:text3xl xl:text-4xl 2xl:text-6xl"
+          >
+            350 <span className="text-white">+</span>
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: "40%" }}
+            whileInView={{ opacity: 1, y: "0" }}
+            transition={{ duration: 0.5, delay: 0.3 }}
+            viewport={{ once: true }}
+            className="text-white text-center text-sm md:text-base 2xl:text-lg"
+          >
+            In savings through our mates rates <br /> discounts
+          </motion.p>
         </div>
-      
+
+        <div className="flex flex-col justify-center items-center ">
+          <motion.p
+            initial={{ opacity: 0, y: "-40%" }}
+            whileInView={{ opacity: 1, y: "0" }}
+            transition={{ duration: 0.5, delay: 0.6 }}
+            viewport={{ once: true }}
+            className="text-[#00F0FF] font-bold text-2xl lg:text3xl xl:text-4xl 2xl:text-6xl"
+          >
+            200 <span className="text-white">+</span>
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: "-40%" }}
+            whileInView={{ opacity: 1, y: "0" }}
+            transition={{ duration: 0.5, delay: 0.6 }}
+            viewport={{ once: true }}
+            className="text-white text-center text-sm md:text-base 2xl:text-lg"
+          >
+            stores you can redeem offers In <br /> person or online
+          </motion.p>
+        </div>
+      </div>
+      <div className="flex items-center justify-center xl:gap-8 md:gap-5 gap-3 mt-10">
+        <div className="flex justify-center items-center" onClick={()=> toast.warning('Winlads IOS app will launch soon')}>
+          <img
+            src={AppStore}
+            alt=""
+            className="cursor-pointer hover:scale-105"
+          />
+        </div>
+        <div className="flex justify-center items-center" onClick={()=> toast.warning('Winlads Android app will launch soon')}>
+          <img
+            src={PlayStore}
+            alt=""
+            className="cursor-pointer hover:scale-105"
+          />
+        </div>
       </div>
     </div>
-  );
+
+    {/* section 02 */}
+
+  </div>  );
 };
 
 export default Apps;
