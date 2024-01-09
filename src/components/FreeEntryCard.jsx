76697dce21aaa45c5d1fbf9ad80bd2cef108fa47
SuraMahesh97 <suranga@botcalm.com>
2024-01-09 05:40:02 +0530

    update

--- src/components/FreeEntryCard.jsx
+++ src/components/FreeEntryCard.jsx
@@ -6,17 +6,17 @@ const FreeEntryCard = () => {
     window.location.href = "https://winlads.com/register?COUPEN=WINFREE";
   };
   return (
-    <div className="rounded-2xl xl:w-64 w-full md:w-72  shadow-lg bg-[#F7B928] shadow-gray-400 relative flex flex-col border-2 border-black">
+    <div className="rounded-2xl xl:w-72 w-full md:w-72  shadow-lg bg-[#F7B928] shadow-gray-400 relative flex flex-col border-2 border-black">
       <div>
         <img
           src={FreeEntry}
           alt=""
-          className="xl:w-64 w-full md:w-72  rounded-l-xl"
+          className="xl:w-72 w-full md:w-72  rounded-l-xl"
         />
       </div>
       <div className="absolute bottom-2 left-0 right-0 mx-2 flex justify-center">
         <button
-          className="bg-[#F7B928] py-2 w-full text-xs 2xl:text-lg hover:font-bold uppercase text-black rounded-lg border border-solid border-black"
+          className="bg-[#F7B928] py-2 w-full hover:scale-105 hover:transition-transform ease-out duration-300 text-xs 2xl:text-lg hover:text-black hover:bg-white uppercase text-black rounded-lg border border-solid border-black"
           onClick={handleClickButton}
         >
           SIGN UP NOW
